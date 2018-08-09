29268c41ce5ab15f5880a5cfb283a023db802767
Bryan Anderson <bryan.texasbobcat@gmail.com>
2018-08-09 14:59:14 -0500

    Refactor1

--- script.js
+++ script.js
@@ -1,58 +1,28 @@
 document.addEventListener('DOMContentLoaded', (ev) => {
-    //console.log('booyah')
-    const appName = document.createElement('h1');
-    document.body.appendChild(appName);
-    appName.textContent = 'Todo App';
-   
-    const input = document.createElement('input');
-    document.body.appendChild(input);
-    input.type = "text";
-    input.value = "What should I do?"
+    h1 = createH1();
+    
+    input = createInput();
+    
+    todoList = createToDoList();
 
-    const todoList = document.createElement('ul');
-    document.body.appendChild(todoList);
-
-
-    const button = document.createElement('button');
-    document.body.appendChild(button);
-    button.textContent = "Add Todo";
-    button.setAttribute('id', 'add_todo');
+    button = createButton();
+    
 
     button.addEventListener('click', (ev) => {
         if (input.value !== "") {
-            const item = document.createElement('li');
+            let item = document.createElement('li');
             item.textContent = input.value;
             
-            const edButton  =   document.createElement('button');
-            edButton.textContent = "Edit";
-            edButton.style.backgroundColor = "pink";
-            edButton.style.cssFloat = "right";
-            edButton.addEventListener('click', (ev) =>{
-                const editInput = document.createElement('input');
-                item.appendChild(editInput);
-                editInput.type = "text";
-                editInput.addEventListener('keyup', (ev) => {
-                    if (ev.keyCode === 13)  {
-                        if (editInput.value !== "") {
-                            item.textContent = editInput.value;
-                            item.appendChild(edButton);
-                            item.appendChild(itButton);
-                        }
-                    }
-                })
-            })
+            //Create X Button, short for item button
+            itButton = createItButton(item);
+
+            //Create Edit Button
+            edButton = createEditButton(item, itButton);
             
-            var itButton = document.createElement('button');
-            itButton.textContent = "X";
-            itButton.style.backgroundColor = "red";
-            itButton.style.cssFloat = "right";
-            itButton.addEventListener('click', (ev) => {
-                item.setAttribute('class', 'completed');
-            });
-            
-            item.appendChild(itButton);
-            item.appendChild(edButton);
+            //Appending things
             todoList.appendChild(item);
+
+            input.value = "";
         } else {
             alert("You have something to do!");
         }
@@ -61,39 +31,19 @@ document.addEventListener('DOMContentLoaded', (ev) => {
     input.addEventListener('keyup', (ev) => {
         if (ev.keyCode === 13) {
             if (input.value !== "") {
-                const item = document.createElement('li');
+                let item = document.createElement('li');
                 item.textContent = input.value;
+                                
+                //Create X Button
+                itButton = createItButton(item);
 
-                const edButton = document.createElement ('button');
-                edButton.textContent = "Edit";
-                edButton.style.backgroundColor = "pink";
-                edButton.style.cssFloat = "right";
-                edButton.addEventListener('click', (ev) =>{
-                    const editInput = document.createElement('input');
-                    item.appendChild(editInput);
-                    editInput.type = "text";
-                    editInput.addEventListener('keyup', (ev) => {
-                        if (ev.keyCode === 13)  {
-                            if (editInput.value !== "") {
-                                item.textContent = editInput.value;
-                                item.appendChild(edButton);
-                                item.appendChild(itButton);
-                            }
-                        }
-                    })
-                })
+                //Create Edit Button
+                edButton = createEditButton(item, itButton);
 
-                const itButton = document.createElement('button');
-                itButton.textContent = "X";
-                itButton.style.backgroundColor = "red";
-                itButton.style.cssFloat = "right";
-                itButton.addEventListener('click', (ev) => {
-                    item.setAttribute('class', 'completed');
-                });
-            
-                item.appendChild(itButton);
-                item.appendChild(edButton);
+                //Append things                                
                 todoList.appendChild(item);
+
+                input.value = "";
             }   else    {
                 alert("You have something to do!")
             }
@@ -103,4 +53,84 @@ document.addEventListener('DOMContentLoaded', (ev) => {
     input.addEventListener('click', (ev) => {
         input.value = "";
     })
+
+    //Variable Creation!
+    function createH1 () {
+        const appName = document.createElement('h1');
+        document.body.appendChild(appName);
+        appName.textContent = 'Todo App';
+        return appName;
+    }
+
+    function createInput () {
+        let input = document.createElement('input');
+        document.body.appendChild(input);
+        input.type = "text";
+        input.value = "What should I do?"
+        return input;
+    }
+
+    function createToDoList ()  {
+        const todoList = document.createElement('ul');
+        document.body.appendChild(todoList);
+        return todoList;
+    }
+
+    function createButton () {
+        const button = document.createElement('button');
+        document.body.appendChild(button);
+        button.textContent = "Add Todo";
+        button.setAttribute('id', 'add_todo');
+        return button;
+    }
+
+
+
+    function createEditButton (item, itButton) {
+        //Create button
+        let edButton  =  document.createElement('button');
+        edButton.textContent = "Edit";
+        edButton.style.backgroundColor = "pink";
+        edButton.style.cssFloat = "right";
+        item.appendChild(edButton);
+
+        //Events
+        edButton.addEventListener('click', (ev) =>{
+            //Create Edit Field
+            let editInput = document.createElement('input');
+            item.appendChild(editInput);
+            editInput.type = "text";
+
+            //Edit List Value
+            editListValue(item, itButton, editInput);
+        })
+        return edButton;
+    }
+
+    //editListValue()
+    function editListValue (item, itButton, editInput) {
+        editInput.addEventListener('keyup', (ev) => {
+            if (ev.keyCode === 13)  {
+                if (editInput.value !== "") {
+                    item.textContent = editInput.value;
+                    item.appendChild(itButton);
+                    item.appendChild(edButton);
+                }
+            }
+        })
+    }
+
+    function createItButton (item)  {
+        //create itbutton
+        const itButton = document.createElement('button');
+            itButton.textContent = "X";
+            itButton.style.backgroundColor = "red";
+            itButton.style.cssFloat = "right";
+            item.appendChild(itButton);
+        //events
+            itButton.addEventListener('click', (ev) => {
+                item.setAttribute('class', 'completed');
+            });
+        return itButton;
+    }
 })
